9ed466c794f188c617331e49343c777954462120
Ruth Georgiev <ruth.georgiev@proton.me>
2024-06-22 10:07:24 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const pets = require('./petList');
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Adopt a Pet!</h1>
+    <p>Browse through the links below to find your new furry friend:</p>
+    <ul>
+      <li><a href="/animals/dogs">Dogs</a></li>
+      <li><a href="/animals/cats">Cats</a></li>
+      <li><a href="/animals/rabbits">Rabbits</a></li>
+    </ul>
+  `);
+});
+
+
+app.get('/animals', (req, res) => {
+  const animalTypes = Object.keys(pets).map(type => ({
+    type: type,
+    animals: pets[type]
+  }));
+  res.json({
+    animalTypes: animalTypes
+  });
+});
+
+
+app.get('/animals/:pet_type', (req, res) => {
+  const petType = req.params.pet_type;
+  const petList = pets[petType];
+
+  if (!petList) {
+    return res.status(404).send('Pet type not found');
+  }
+
+  const petItems = petList.map((pet, index) => {
+    return `<li><a href="/animals/${petType}/${index}">${pet.name}</a></li>`;
+  }).join('');
+
+  res.send(`
+    <h1>List of ${petType.charAt(0).toUpperCase() + petType.slice(1)}</h1>
+    <ul>${petItems}</ul>
+  `);
+});
+
+
+app.get('/animals/:pet_type/:pet_id', (req, res) => {
+    const petType = req.params.pet_type;
+    const petId = parseInt(req.params.pet_id);
+    const petList = pets[petType];
+  
+    if (!petList || petId < 0 || petId >= petList.length) {
+      return res.status(404).send('Pet not found');
+    }
+  
+    const pet = petList[petId];
+  
+    
+    const html = `
+      <h1>${pet.name}</h1>
+      <img src="${pet.url}" alt="${pet.name}" width="500">
+      <p>${pet.description}</p>
+      <ul>
+        <li>Breed: ${pet.breed}</li>
+        <li>Age: ${pet.age}</li>
+      </ul>
+      <a href="/animals/${petType}">Back to ${petType}</a>
+    `;
+  
+    
+    res.send(html);
+  });
+  
